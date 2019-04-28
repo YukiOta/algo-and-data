9c8d950766eecfd732edf7b38aa7e71183dd9770
YukiOta <yuki.ota@ruri.waseda.jp>
2019-04-28 09:10:53 +0900

    [modified] 5.3 binary search

--- ch05/ALDS1_4_B_binary_search.cpp
+++ ch05/ALDS1_4_B_binary_search.cpp
@@ -1,42 +1,78 @@
-// 5.2
-// linear search
+// 5.3
+// binary search
+
+// *---
+// STL用いる例
+// *---
 
 #include <cstdio>
+#include <algorithm>
+using namespace std;
 
-bool binarySearch(int A[], int n, int key)
-{
-    int left = 0, right = n;
-    int mid;
-    while (left < right)
-    {
-        mid = ( left + right ) / 2;
-        if (A[mid] == key) return true;
-        if (key < A[mid]) right = mid;
-        // + 1が重要
-        else if(key > A[mid]) left = mid + 1;
-    }
-    return false;
-}
+int A[1000000], n, sum;
 
 int main()
 {
-    int A[1000000], i, n, q, t;
-    int sum = 0;
-
     scanf("%d", &n);
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &A[i]);
     }
 
+    int q, k;
     scanf("%d", &q);
-    for (i = 0; i < q; i++)
+    for (int i = 0; i < q; i++)
     {
-        scanf("%d", &t);
-        if (binarySearch(A, n, t)) sum++;
+        scanf("%d", &k);
+        if (*lower_bound(A, A + n, k) == k)
+            sum++;
     }
 
-    printf("%d\n", sum);
+    printf("%d", sum);
 
     return 0;
 }
+
+// *---
+// scratchでやる例
+// *---
+
+// #include <cstdio>
+
+// bool binarySearch(int A[], int n, int key)
+// {
+//     int left = 0, right = n;
+//     int mid;
+//     while (left < right)
+//     {
+//         mid = ( left + right ) / 2;
+//         if (A[mid] == key) return true;
+//         if (key < A[mid]) right = mid;
+//         // + 1が重要
+//         else if(key > A[mid]) left = mid + 1;
+//     }
+//     return false;
+// }
+
+// int main()
+// {
+//     int A[1000000], i, n, q, t;
+//     int sum = 0;
+
+//     scanf("%d", &n);
+//     for (i = 0; i < n; i++)
+//     {
+//         scanf("%d", &A[i]);
+//     }
+
+//     scanf("%d", &q);
+//     for (i = 0; i < q; i++)
+//     {
+//         scanf("%d", &t);
+//         if (binarySearch(A, n, t)) sum++;
+//     }
+
+//     printf("%d\n", sum);
+
+//     return 0;
+// }
