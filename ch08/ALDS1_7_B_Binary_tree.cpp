3b251cb17e5d1a08b92358bbd3b3d2e60fb9c128
YukiOta <yuki.ota@ruri.waseda.jp>
2019-05-07 22:57:30 +0900

    [add] 8.3 Binary tree

---
+++ ch08/ALDS1_7_B_Binary_tree.cpp
@@ -0,0 +1,101 @@
+// 8.3
+// 二分木の表現
+
+#include <cstdio>
+#define MAX 1000
+#define NIL -1
+
+struct Node
+{
+    int parent, left, right;
+};
+
+Node T[MAX];
+int n, D[MAX], H[MAX];
+
+void setDepth(int u, int d)
+{
+    if ( u == NIL ) return;
+    D[u] = d;
+    setDepth(T[u].right, d + 1);
+    setDepth(T[u].left, d + 1);
+}
+
+int setHeight(int u)
+{
+    int h1 = 0, h2 = 0;
+    if ( T[u].left != NIL )
+        h1 = setHeight(T[u].left) + 1;
+    if ( T[u].right != NIL )
+        h2 = setHeight(T[u].right) + 1;
+    return H[u] = ( h1 > h2 ? h1 : h2);
+}
+
+int getSibling(int u)
+{
+    if ( T[u].parent == NIL ) return NIL;
+    int parent = T[u].parent;
+    if (T[parent].left != u && T[parent].left != NIL )
+        return T[parent].left;
+    if (T[parent].right != u && T[parent].right != NIL )
+        return T[parent].right;
+    return NIL;
+}
+
+int getDegree(int u)
+{
+    int deg = 0;
+    if ( T[u].left != NIL ) deg++;
+    if ( T[u].right != NIL ) deg++;
+    return deg;
+}
+
+void print(int u)
+{
+    printf("node %d: ", u);
+    printf("parent = %d, ", T[u].parent);
+    printf("sibling = %d, ", getSibling(u));
+    printf("degree = %d, ", getDegree(u));
+    printf("depth = %d, ", D[u]);
+    printf("height = %d, ", H[u]);
+    if ( T[u].parent == NIL )
+        printf("root\n");
+    else if (T[u].left == NIL && T[u].right == NIL)
+        printf("leaf\n");
+    else
+        printf("internal node\n");
+
+}
+
+int main()
+{
+
+    int v, l, r, root = 0;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        T[i].parent = NIL;
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d %d", &v, &l, &r);
+        T[v].left = l;
+        T[v].right = r;
+        if (l != NIL)
+            T[l].parent = v;
+        if (r != NIL)
+            T[r].parent = v;
+    }
+
+    for (int i = 0; i < n; i++)
+        if ( T[i].parent == NIL)
+            root = i;
+    
+    setDepth(root, 0);
+    setHeight(root);
+
+    for (int i = 0; i < n; i++)
+        print(i);
+
+    return 0;
+}
