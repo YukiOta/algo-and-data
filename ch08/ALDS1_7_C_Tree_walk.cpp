37def2159ee7727c28650aaa847f38610c0ff3a4
YukiOta <yuki.ota@ruri.waseda.jp>
2019-05-08 23:13:27 +0900

    [add] 8.7 tree walk

---
+++ ch08/ALDS1_7_C_Tree_walk.cpp
@@ -0,0 +1,80 @@
+// 8.7
+// 木の巡回
+
+#include <cstdio>
+#define MAX 10000
+#define NIL -1
+
+struct Node
+{
+    int parent, left, right;
+};
+
+Node T[MAX];
+int n;
+
+void preParse(int u)
+{
+    if (u == NIL)
+        return;
+    printf(" %d", u);
+    preParse(T[u].left);
+    preParse(T[u].right);
+}
+
+void inParse(int u)
+{
+    if (u == NIL)
+        return;
+    inParse(T[u].left);
+    printf(" %d", u);
+    inParse(T[u].right);
+}
+
+void postParse(int u)
+{
+    if (u == NIL)
+        return;
+    postParse(T[u].left);
+    postParse(T[u].right);
+    printf(" %d", u);
+}
+
+int main()
+{
+    int v, l, r;
+    int root;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        T[i].parent = NIL;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d %d", &v, &l, &r);
+        T[v].left = l;
+        T[v].right = r;
+        if (l != NIL)
+            T[l].parent = v;
+        if (r != NIL)
+            T[r].parent = v;
+    }
+
+    for (int i = 0; i < n; i++)
+        if (T[i].parent == NIL)
+            root = i;
+    
+    printf("Preorder\n");
+    preParse(root);
+    printf("\n");
+    printf("Inorder\n");
+    inParse(root);
+    printf("\n");
+    printf("Postorder\n");
+    postParse(root);
+    printf("\n");
+
+    return 0;
+}
